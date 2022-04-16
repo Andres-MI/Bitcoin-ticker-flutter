66f2e5e651c91b330a20388cac23d015a303be13
Andres Melenchon <andres.melenchon88@gmail.com>
2022-04-16 22:53:46 +0200

    Multiple crypto coin. Gold Challenge. WIP: Testing

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -4,41 +4,50 @@ import 'package:bitcoin_ticker/coin_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'crypto_card.dart';
+
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-
-  String selectedCurrency = 'USD';
-  String totalExchange = '?';
+  String selectedCurrency = 'AUD';
+  List<String> cryptoCurrencies = cryptoList;
+  List<String> totalExchange = [];
   CoinData coinData = CoinData();
+  bool isWaiting = false;
 
-  CupertinoPicker iOSPicker(){
+  CupertinoPicker iOSPicker() {
     List<Text> pickerItems = [];
-    for(String currency in currenciesList){
+    for (String currency in currenciesList) {
       pickerItems.add(Text(currency));
     }
-    return CupertinoPicker(itemExtent: 32.0, onSelectedItemChanged: (selectedIndex){
-      selectedCurrency = currenciesList[selectedIndex];
-      updateUI();
-    }, children: pickerItems);
+    return CupertinoPicker(
+        itemExtent: 32.0,
+        onSelectedItemChanged: (selectedIndex) {
+          selectedCurrency = currenciesList[selectedIndex];
+          getData();
+        },
+        children: pickerItems);
   }
 
-  DropdownButton androidDropdown(){
-    List <DropdownMenuItem<String>> dropdownItems = [];
+  DropdownButton androidDropdown() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
     for (String currency in currenciesList) {
-      var newItem = DropdownMenuItem(child: Text(currency), value: currency,);
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
       dropdownItems.add(newItem);
     }
-     return DropdownButton<String>(
+    return DropdownButton<String>(
         value: selectedCurrency,
         items: dropdownItems,
         onChanged: (value) {
           setState(() {
             selectedCurrency = value!;
-            updateUI();
+            getData();
           });
         });
   }
@@ -46,7 +55,7 @@ class _PriceScreenState extends State<PriceScreen> {
   @override
   void initState() {
     super.initState();
-    updateUI();
+    getData();
   }
 
   @override
@@ -59,47 +68,36 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = $totalExchange $selectedCurrency',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+          for (int i = 0; i < cryptoList.length; i++)
+            CryptoCard(
+              totalExchange: isWaiting ? '?' : totalExchange[i],
+              selectedCurrency: selectedCurrency,
+              cryptoCurrency: cryptoList[i],
             ),
-          ),
           Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: Platform.isIOS ? iOSPicker() : androidDropdown()
-          ),
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: Platform.isIOS ? iOSPicker() : androidDropdown()),
         ],
       ),
     );
   }
 
-  Future<void> updateUI() async {
-    var exchangeData = await coinData.getCoinData(crypto: 'BTC', fiat: selectedCurrency);
-    if(exchangeData == null){
-      totalExchange = '??';
-    } else {
-      setState(() {
-        totalExchange = exchangeData.toStringAsFixed(2);
-      });
+  Future<void> getData() async {
+    isWaiting = true;
+    totalExchange = [];
+    for (String crypto in cryptoList) {
+      var exchangeData =
+          await coinData.getCoinData(crypto: crypto, fiat: selectedCurrency);
+      if (exchangeData == null) {
+        totalExchange.add('??');
+      } else {
+        totalExchange.add(exchangeData.toStringAsFixed(2));
+      }
     }
+    isWaiting = false;
+    setState(() {});
   }
 }
